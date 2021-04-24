c6c5f6f1496b6212013a1b8f2b049996c3ecf9c3
William <wcortez799@hotmail.com>
2021-04-24 09:01:04 -0600

    Update proyecto clase automatizacion

---
+++ Automatizacion/src/test/java/Sinap/Automatizacion/App.java
@@ -0,0 +1,13 @@
+package Sinap.Automatizacion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Automatizacion/src/test/java/Sinap/Test/RunnersFeature.java
@@ -0,0 +1,12 @@
+package Sinap.Test;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = { "src/test/resources/features" }, glue = { "Sinap.Definitions" })
+
+public class RunnersFeature {
+
+}
